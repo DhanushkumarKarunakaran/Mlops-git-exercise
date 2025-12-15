dee33cbdb12b74d8f9167158f02b93a8b718230b
DhanushkumarKarunakaran <d80093076@gmail.com>
2025-12-15 13:16:52 +0530

    Create git.py

---
+++ git.py
@@ -0,0 +1,7 @@
+class HelloWorld:
+    def main():
+        print("Hello World from Git")
+        print("This code was edited directly on GitHub")
+
+if __name__ == "__main__":
+    HelloWorld.main()
